02502ac52e308242deb09d1074a0e0f5234de2b7
hongshibao <shibaohong@outlook.com>
2016-06-05 12:13:14 +0800

    Add comments

--- kdtree.go
+++ kdtree.go
@@ -7,9 +7,13 @@ import (
 )
 
 type Point interface {
+	// Return the total number of dimensions
 	Dim() int
+	// Return the value X_{dim}, dim is started from 0
 	GetValue(dim int) float64
+	// Return the distance between two points
 	Distance(p Point) float64
+	// Return the distance between the point and the plane X_{dim}=val
 	PlaneDistance(val float64, dim int) float64
 }
 
